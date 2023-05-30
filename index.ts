ef81e88e34f3f0408f2695c2395351d4006620a5
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-05-30 11:27:30 +0300

    first commit

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import NodeCache from 'node-cache'
 import { Boom } from '@hapi/boom'
-import makeWASocket, { BufferJSON, AnyMessageContent, delay, DisconnectReason,fetchLatestBaileysVersion, getAggregateVotesInPollMessage, makeCacheableSignalKeyStore, makeInMemoryStore, proto, useMultiFileAuthState, WAMessageContent, WAMessageKey } from '@whiskeysockets/baileys'
-import MAIN_LOGGER from '@whiskeysockets/baileys/lib/Utils/logger'
+import makeWASocket, {  AnyMessageContent, delay, DisconnectReason,fetchLatestBaileysVersion, getAggregateVotesInPollMessage, makeCacheableSignalKeyStore, makeInMemoryStore, proto, useMultiFileAuthState, WAMessageContent, WAMessageKey } from '@whiskeysockets/baileys'
 import express from 'express';
-const logger = MAIN_LOGGER.child({})
-logger.level = 'trace'
+import P  from 'pino';
 const msgRetryCounterCache = new NodeCache()
+const logger = P({ level: 'silent' })
 const store = makeInMemoryStore({ logger });
 store?.readFromFile('./baileys_store_multi.json')
 // save every 10s
@@ -53,134 +52,134 @@ const startSock = async () => {
 
 	// the process function lets you process all events that just occurred
 	// efficiently in a batch
-	sock.ev.process(
-		// events is a map for event name => event data
-		async (events) => {
-			// something about the connection changed
-			// maybe it closed, or we received all offline message or connection opened
-			if (events['connection.update']) {
-				const update = events['connection.update']
-				const { connection, lastDisconnect } = update
-				if (connection === 'close') {
-					// reconnect if not logged out
-					if ((lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
-						startSock()
-					} else {
-						console.log('Connection closed. You are logged out.')
-					}
-				}
+	// sock.ev.process(
+	// 	// events is a map for event name => event data
+	// 	async (events) => {
+	// 		// something about the connection changed
+	// 		// maybe it closed, or we received all offline message or connection opened
+	// 		if (events['connection.update']) {
+	// 			const update = events['connection.update']
+	// 			const { connection, lastDisconnect } = update
+	// 			if (connection === 'close') {
+	// 				// reconnect if not logged out
+	// 				if ((lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
+	// 					startSock()
+	// 				} else {
+	// 					console.log('Connection closed. You are logged out.')
+	// 				}
+	// 			}
 
-				console.log('connection update', update)
-			}
+	// 			console.log('connection update', update)
+	// 		}
 
-			// credentials updated -- save them
-			if (events['creds.update']) {
-				await saveCreds()
-			}
+	// 		// credentials updated -- save them
+	// 		if (events['creds.update']) {
+	// 			await saveCreds()
+	// 		}
 
-			if (events['labels.association']) {
-				console.log(events['labels.association'])
-			}
+	// 		if (events['labels.association']) {
+	// 			console.log(events['labels.association'])
+	// 		}
 
 
-			if (events['labels.edit']) {
-				console.log(events['labels.edit'])
-			}
+	// 		if (events['labels.edit']) {
+	// 			console.log(events['labels.edit'])
+	// 		}
 
-			if (events.call) {
-				console.log('recv call event', events.call)
-			}
+	// 		if (events.call) {
+	// 			console.log('recv call event', events.call)
+	// 		}
 
-			// history received
-			if (events['messaging-history.set']) {
-				const { chats, contacts, messages, isLatest } = events['messaging-history.set']
-				console.log(`recv ${chats.length} chats, ${contacts.length} contacts, ${messages.length} msgs (is latest: ${isLatest})`)
-			}
+	// 		// history received
+	// 		if (events['messaging-history.set']) {
+	// 			const { chats, contacts, messages, isLatest } = events['messaging-history.set']
+	// 			console.log(`recv ${chats.length} chats, ${contacts.length} contacts, ${messages.length} msgs (is latest: ${isLatest})`)
+	// 		}
 
-			// received a new message
-			if (events['messages.upsert']) {
-				const upsert = events['messages.upsert']
-				console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
+	// 		// received a new message
+	// 		// if (events['messages.upsert']) {
+	// 		// 	const upsert = events['messages.upsert']
+	// 		// 	console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
 
-				if (upsert.type === 'notify') {
-					for (const msg of upsert.messages) {
-						if (!msg.key.fromMe) {
-							console.log('replying to', msg.key.remoteJid)
-							await sock!.readMessages([msg.key])
-							console.log('read messages')
-							console.log('read messages')
-							console.log('read messages')
-							console.log('read messages')
-							console.log(msg)
-							console.log('read messages')
-							console.log('read messages')
-							console.log('read messages')
-							console.log('read messages')
-							console.log('read messages')
-							console.log('read messages')
-							console.log('read messages')
-							await sendMessageWTyping({ text: 'Hello there!' }, msg.key.remoteJid!)
-						}
-					}
-				}
-			}
+	// 		// 	if (upsert.type === 'notify') {
+	// 		// 		for (const msg of upsert.messages) {
+	// 		// 			if (!msg.key.fromMe) {
+	// 		// 				console.log('replying to', msg.key.remoteJid)
+	// 		// 				await sock!.readMessages([msg.key])
+	// 		// 				console.log('read messages')
+	// 		// 				console.log('read messages')
+	// 		// 				console.log('read messages')
+	// 		// 				console.log('read messages')
+	// 		// 				console.log(msg)
+	// 		// 				console.log('read messages')
+	// 		// 				console.log('read messages')
+	// 		// 				console.log('read messages')
+	// 		// 				console.log('read messages')
+	// 		// 				console.log('read messages')
+	// 		// 				console.log('read messages')
+	// 		// 				console.log('read messages')
+	// 		// 				await sendMessageWTyping({ text: 'Hello there!' }, msg.key.remoteJid!)
+	// 		// 			}
+	// 		// 		}
+	// 		// 	}
+	// 		// }
 
-			// messages updated like status delivered, message deleted etc.
-			if (events['messages.update']) {
-				console.log(
-					JSON.stringify(events['messages.update'], undefined, 2)
-				)
+	// 		// messages updated like status delivered, message deleted etc.
+	// 		if (events['messages.update']) {
+	// 			console.log(
+	// 				JSON.stringify(events['messages.update'], undefined, 2)
+	// 			)
 
-				for (const { key, update } of events['messages.update']) {
-					if (update.pollUpdates) {
-						const pollCreation = await getMessage(key)
-						if (pollCreation) {
-							console.log(
-								'got poll update, aggregation: ',
-								getAggregateVotesInPollMessage({
-									message: pollCreation,
-									pollUpdates: update.pollUpdates,
-								})
-							)
-						}
-					}
-				}
-			}
+	// 			for (const { key, update } of events['messages.update']) {
+	// 				if (update.pollUpdates) {
+	// 					const pollCreation = await getMessage(key)
+	// 					if (pollCreation) {
+	// 						console.log(
+	// 							'got poll update, aggregation: ',
+	// 							getAggregateVotesInPollMessage({
+	// 								message: pollCreation,
+	// 								pollUpdates: update.pollUpdates,
+	// 							})
+	// 						)
+	// 					}
+	// 				}
+	// 			}
+	// 		}
 
-			if (events['message-receipt.update']) {
-				console.log(events['message-receipt.update'])
-			}
+	// 		if (events['message-receipt.update']) {
+	// 			console.log(events['message-receipt.update'])
+	// 		}
 
-			if (events['messages.reaction']) {
-				console.log(events['messages.reaction'])
-			}
+	// 		if (events['messages.reaction']) {
+	// 			console.log(events['messages.reaction'])
+	// 		}
 
-			if (events['presence.update']) {
-				console.log(events['presence.update'])
-			}
+	// 		if (events['presence.update']) {
+	// 			console.log(events['presence.update'])
+	// 		}
 
-			if (events['chats.update']) {
-				console.log(events['chats.update'])
-			}
+	// 		if (events['chats.update']) {
+	// 			console.log(events['chats.update'])
+	// 		}
 
-			if (events['contacts.update']) {
-				for (const contact of events['contacts.update']) {
-					if (typeof contact.imgUrl !== 'undefined') {
-						const newUrl = contact.imgUrl === null
-							? null
-							: await sock!.profilePictureUrl(contact.id!).catch(() => null)
-						console.log(
-							`contact ${contact.id} has a new profile pic: ${newUrl}`,
-						)
-					}
-				}
-			}
+	// 		if (events['contacts.update']) {
+	// 			for (const contact of events['contacts.update']) {
+	// 				if (typeof contact.imgUrl !== 'undefined') {
+	// 					const newUrl = contact.imgUrl === null
+	// 						? null
+	// 						: await sock!.profilePictureUrl(contact.id!).catch(() => null)
+	// 					console.log(
+	// 						`contact ${contact.id} has a new profile pic: ${newUrl}`,
+	// 					)
+	// 				}
+	// 			}
+	// 		}
 
-			if (events['chats.delete']) {
-				console.log('chats deleted ', events['chats.delete'])
-			}
-		}
-	)
+	// 		if (events['chats.delete']) {
+	// 			console.log('chats deleted ', events['chats.delete'])
+	// 		}
+	// 	}
+	// )
 
 	return sock
 
